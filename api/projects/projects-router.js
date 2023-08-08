e2ae1d279e078385e3cff1141f42bc2c58003a0c
Sarmad Ashkuri <sarmadashkuri@gmail.com>
2023-08-08 01:18:56 -0400

    finished projects endpoints

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -22,22 +22,45 @@ router.get('/:id', validateProjectId, (req, res) => {
 });
 
 router.post('/', validateProjectInput, (req, res, next) => {
-    // console.log(req.body);
-    Project.insert({ name: req.body.name, description: req.body.description })
+    Project.insert(req.body)
         .then(newProject => {
-            res.status(201).json(newProject);
             console.log(newProject);
+            res.status(201).json(newProject);
         })
         .catch(next);
 });
 
 router.put('/:id', validateProjectId, (req, res, next) => {
+    const { name, description, completed } = req.body;
+    console.log(req.body);
+    if (!name || !description) {
+        res.status(400).json({ message: 'please fill in the required fields'});
+    } else if (completed === undefined) {
+        res.status(400).json({ message: 'Please provide the completed field' });
+    } else {
+        Project.update(req.params.id, req.body)
+        .then(updatedProject => {
+            res.status(200).json(updatedProject)
+            console.log(updatedProject);
+        })
+        .catch(next);
+    }
 });
 
 router.delete('/:id', validateProjectId, (req, res, next) => {
+    Project.remove(req.params.id,)
+        .then(() => {
+            res.json(req.project);
+        })
+        .catch(next);
 });
 
 router.get('/:id/actions', validateProjectId, (req, res, next) => {
+    Project.getProjectActions(req.params.id)
+        .then(actions => {
+            res.json(actions)
+        })
+        .catch(next);
 });
 
 router.use((err, req, res, next) => {
